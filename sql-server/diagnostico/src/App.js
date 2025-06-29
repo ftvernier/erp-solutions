7ab795d5965f39f8aa2a443e2987b9b27a7bd5c7
Fernando Tadeu Vernier <Fernando.vernier@hotmail.com>
2025-06-29 12:12:28 -0300

    Create App.js

---
+++ sql-server/diagnostico/src/App.js
@@ -0,0 +1,11 @@
+import SQLDiagnosticGenerator from './SQLDiagnosticGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <SQLDiagnosticGenerator />
+    </div>
+  );
+}
+
+export default App;
