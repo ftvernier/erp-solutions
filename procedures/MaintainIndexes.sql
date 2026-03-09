ab9557d1309f86d4f6552ff941f32c35936530d5
Fernando Tadeu Vernier <37316301+ftvernier@users.noreply.github.com>
2026-03-09 13:10:32 -0300

    Atualização para não considerar as tabelas SYS_

--- procedures/MaintainIndexes.sql
+++ procedures/MaintainIndexes.sql
@@ -10,7 +10,6 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    -- Declaração de variáveis
     DECLARE @SchemaName NVARCHAR(256);
     DECLARE @TableName NVARCHAR(256);
     DECLARE @IndexName NVARCHAR(256);
@@ -21,7 +20,6 @@ BEGIN
     DECLARE @EndTime DATETIMEOFFSET;
     DECLARE @ElapsedTime NVARCHAR(50);
 
-    -- Cursor para iterar sobre índices fragmentados
     DECLARE IndexCursor CURSOR FOR
     SELECT 
         SCHEMA_NAME(o.schema_id) AS SchemaName,
@@ -38,46 +36,38 @@ BEGIN
         AND ips.page_count > 100
         AND i.type_desc NOT IN ('XML', 'SPATIAL', 'FULLTEXT')
         AND i.is_disabled = 0
+        --EXCLUSÃO DAS TABELAS SYS_ DO PROTHEUS
+        AND OBJECT_NAME(ips.object_id) NOT LIKE 'SYS[_]%'
     ORDER BY 
         avg_fragmentation_in_percent DESC;
 
     OPEN IndexCursor;
-
     FETCH NEXT FROM IndexCursor INTO @SchemaName, @TableName, @IndexName, @IndexID, @Fragmentation;
 
     WHILE @@FETCH_STATUS = 0
     BEGIN
-        -- Captura o horário de início do servidor
         SET @StartTime = SYSDATETIMEOFFSET();
 
         BEGIN TRANSACTION;
         BEGIN TRY
             IF @Fragmentation >= 30
             BEGIN
-                -- Rebuild Index
                 SET @SQL = 'ALTER INDEX [' + @IndexName + '] ON [' + @SchemaName + '].[' + @TableName + '] REBUILD;';
             END
             ELSE IF @Fragmentation >= 5 AND @Fragmentation < 30
             BEGIN
-                -- Reorganize Index
                 SET @SQL = 'ALTER INDEX [' + @IndexName + '] ON [' + @SchemaName + '].[' + @TableName + '] REORGANIZE;';
             END
 
-            -- Executa o comando
             EXEC sp_executesql @SQL;
 
-            -- Captura o horário de término do servidor
             SET @EndTime = SYSDATETIMEOFFSET();
-
-            -- Calcula o tempo decorrido
             SET @ElapsedTime = CONVERT(NVARCHAR(50), DATEDIFF(SECOND, @StartTime, @EndTime)) + ' seconds';
 
-            -- Imprime os resultados
             PRINT 'Executed: ' + @SQL + ' | Start Time: ' + CONVERT(NVARCHAR, @StartTime, 121) +
                   ' | End Time: ' + CONVERT(NVARCHAR, @EndTime, 121) +
                   ' | Elapsed Time: ' + @ElapsedTime;
 
-            -- Commit da transação
             COMMIT TRANSACTION;
         END TRY
         BEGIN CATCH
@@ -88,7 +78,6 @@ BEGIN
         FETCH NEXT FROM IndexCursor INTO @SchemaName, @TableName, @IndexName, @IndexID, @Fragmentation;
     END;
 
-    -- Encerramento do Cursor
     BEGIN TRY
         CLOSE IndexCursor;
         DEALLOCATE IndexCursor;
